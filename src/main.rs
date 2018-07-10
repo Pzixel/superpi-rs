2b8874c0bff2f480cdf299bb8e17d58218f2dc18
Psilon <pzixel@gmail.com>
2018-07-10 22:18:22 +0300

    Make it compile

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,32 @@
 extern crate num_rational;
 extern crate num_traits;
-use num_rational::BigRational;
-use num_traits::cast::FromPrimitive;
+extern crate num_bigint;
+
+use num_rational::Ratio;
+use num_bigint::BigInt;
 use std::time::Instant;
+
+type BigRat = Ratio<BigInt>;
 
 fn main() {
     let n: u32 = std::env::args().skip(1).next().expect("provide numbers of iterations").parse().unwrap();
 
-    let mut a = BigRational::from_i32(1).unwrap();
-    let mut b = BigRational::from_f64(std::f64::consts::FRAC_1_SQRT_2).unwrap();
-    let mut t = BigRational::new(1.into(), 4.into());
-    let mut p = BigRational::from_i32(1).unwrap();
-    let two = BigRational::from_i32(2).unwrap();
+    let mut a = BigRat::from_integer(1_u64.into());
+    let mut b = BigRat::new(470_832_u64.into(), 665_857_u64.into());
+    let mut t = BigRat::new(1_u64.into(), 4_u64.into());
+    let mut p = BigRat::from_integer(1_u64.into());
 
     let now = Instant::now();
+    let half = BigRat::new(1_u64.into(), 2_u64.into());
+    let two = BigRat::new(2_u64.into(), 1_u64.into());
 
     for _ in 0..n {
-        let new_a = (a + b)/two;
-        let new_b = (a*b).sqrt();
-        let a_diff = a - new_a;
-        let new_t = t - p*a_diff*a_diff;
-        let new_p = two*p;
+        let new_a = (a.clone() + b.clone())*half.clone();
+        let new_b_square = a.clone()*b;
+        let new_b = BigRat::new(new_b_square.numer().sqrt(),new_b_square.denom().sqrt());
+        let a_diff = a - new_a.clone();
+        let new_t = t - p.clone()*a_diff.clone()*a_diff;
+        let new_p = two.clone()*p;
 
         a = new_a;
         b = new_b;
@@ -28,5 +34,7 @@ fn main() {
         p = new_p;
     }
 
-    println!("Elapsed time: {}{}ms", now.elapsed().as_secs(), now.elapsed().subsec_millis());
+    let a_plus_b = a+b;
+    let result = a_plus_b.clone()*a_plus_b/(BigRat::from_integer(4_u64.into())*t);
+    println!("Elapsed time: {}{}ms. Result = {}", now.elapsed().as_secs(), now.elapsed().subsec_millis(), result);
 }
