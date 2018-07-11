ff8ef73c1d47f7fc62b5b2bf185ffa0cd0ca8105
Psilon <pzixel@gmail.com>
2018-07-11 13:37:07 +0300

    remove clones

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,33 @@
 extern crate num_rational;
 extern crate num_traits;
-extern crate num_bigint;
 
-use num_rational::Ratio;
-use num_bigint::BigInt;
+use num_rational::BigRational;
 use std::time::Instant;
 
-type BigRat = Ratio<BigInt>;
-
 fn main() {
-    let n: u32 = std::env::args().skip(1).next().expect("provide numbers of iterations").parse().unwrap();
+    let n: u32 = std::env::args()
+        .skip(1)
+        .next()
+        .expect("provide numbers of iterations")
+        .parse()
+        .unwrap();
 
-    let mut a = BigRat::from_integer(1_u64.into());
-    let mut b = BigRat::new(470_832_u64.into(), 665_857_u64.into());
-    let mut t = BigRat::new(1_u64.into(), 4_u64.into());
-    let mut p = BigRat::from_integer(1_u64.into());
+    let mut a = BigRational::from_integer(1_u64.into());
+    let mut b = BigRational::new(470_832_u64.into(), 665_857_u64.into());
+    let mut t = BigRational::new(1_u64.into(), 4_u64.into());
+    let mut p = BigRational::from_integer(1_u64.into());
 
     let now = Instant::now();
-    let half = BigRat::new(1_u64.into(), 2_u64.into());
-    let two = BigRat::new(2_u64.into(), 1_u64.into());
+    let half = BigRational::new(1_u64.into(), 2_u64.into());
+    let two = BigRational::new(2_u64.into(), 1_u64.into());
 
     for _ in 0..n {
-        let new_a = (a.clone() + b.clone())*half.clone();
-        let new_b_square = a.clone()*b;
-        let new_b = BigRat::new(new_b_square.numer().sqrt(),new_b_square.denom().sqrt());
-        let a_diff = a - new_a.clone();
-        let new_t = t - p.clone()*a_diff.clone()*a_diff;
-        let new_p = two.clone()*p;
+        let new_a = (&a + &b) * &half;
+        let new_b_square = &a * b;
+        let new_b = BigRational::new(new_b_square.numer().sqrt(), new_b_square.denom().sqrt());
+        let a_diff = a - &new_a;
+        let new_t = t - &p * &a_diff * a_diff;
+        let new_p = &two * p;
 
         a = new_a;
         b = new_b;
@@ -34,7 +35,12 @@ fn main() {
         p = new_p;
     }
 
-    let a_plus_b = a+b;
-    let result = a_plus_b.clone()*a_plus_b/(BigRat::from_integer(4_u64.into())*t);
-    println!("Elapsed time: {}{}ms. Result = {}", now.elapsed().as_secs(), now.elapsed().subsec_millis(), result);
+    let a_plus_b = a + b;
+    let result = &a_plus_b * &a_plus_b / (BigRational::from_integer(4_u64.into()) * t);
+    println!(
+        "Result: {}\nElapsed time: {}s {}ms.",
+        result,
+        now.elapsed().as_secs(),
+        now.elapsed().subsec_millis()
+    );
 }
